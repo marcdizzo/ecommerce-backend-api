000d2dc89dc5fb3d680c4ee6ed5bdd40f1beb534
marc dizzo <marcmarkion456@gmail.com>
2023-07-02 18:00:41 +0300

    build five

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const mongoose = require("mongoose")
 
 // const connectionStr = 'mongodb://127.0.0.1:27017/ecommercebackend'
-const connectionStr = 'mongodb+srv://mark:mark@cluster0.ecmc4wf.mongodb.net/ecommercebackend'
+const connectionStr = 'mongodb+srv://mark:mark@cluster0.ecmc4wf.mongodb.net/ecommerce'
 
 mongoose.connect(connectionStr, {useNewUrlParser:true})
 .then(() => console.log("connected to mongodb"))
