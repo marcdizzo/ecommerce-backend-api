9a91fb0170e96bf53886fe1cb1fb82b4e1902af7
marc dizzo <marcmarkion456@gmail.com>
2023-07-02 18:10:20 +0300

    Changed database name

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const mongoose = require("mongoose")
 
 // const connectionStr = 'mongodb://127.0.0.1:27017/ecommercebackend'
-const connectionStr = 'mongodb+srv://mark:mark@cluster0.ecmc4wf.mongodb.net/ecommerce'
+const connectionStr = 'mongodb+srv://mark:mark@cluster0.ecmc4wf.mongodb.net/ecommerce-data'
 
 mongoose.connect(connectionStr, {useNewUrlParser:true})
 .then(() => console.log("connected to mongodb"))
